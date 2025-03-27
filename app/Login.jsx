e0728ed32e83bb03742731b02c00e4d66f01adf2
戴森球 <3289990188@qq.com>
2025-03-27 21:27:29 +0800

    配置Icon组件

--- app/Login.jsx
+++ app/Login.jsx
@@ -1,9 +1,15 @@
 import {View,Text} from 'react-native'
+import Icon from '../assets/icons';
 
 const Login=()=>{
     return(
         <View>
             <Text>Login</Text>
+            <Text>Login</Text>
+            <Text>Login</Text>
+            <Text>Login</Text>
+            <Text>Login</Text>
+            <Icon name="lock" color={'red'} size={26} strokeWidth={1.6} />
         </View>
     )
 }
--- app/signUp.jsx
+++ app/signUp.jsx
@@ -4,6 +4,8 @@ const signUp=()=>{
     return(
         <View>
             <Text>signUp</Text>
+            <Text>signUp</Text>
+            <Text>signUp</Text>
         </View>
     )
 }
