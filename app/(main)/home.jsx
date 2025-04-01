042846249bca6bd257034ef9415f7612e62300f1
戴森球 <3289990188@qq.com>
2025-04-01 15:29:03 +0800

    帖子详情，以及评论功能

--- app/(main)/home.jsx
+++ app/(main)/home.jsx
@@ -1,20 +1,20 @@
-import { View, Text, StyleSheet, Pressable,ScrollView,FlatList,Button,Alert } from 'react-native'
-import React,{useEffect,useState} from 'react'
-import { useAuth } from '../../contexts/AuthContext'
-import { hp, wp } from '../../helpers/common'
-import { theme } from '../../constants/theme'
+import { View, Text, Button, Alert, StyleSheet, Pressable, ScrollView, FlatList } from 'react-native'
+import React, { useEffect, useState } from 'react'
 import ScreenWrapper from '../../components/ScreenWrapper'
-import { useRouter } from 'expo-router'
-import  Icon  from '../../assets/icons'
-import {Svg,Circle,Path} from 'react-native-svg'
-import { ImageBackground } from 'expo-image'
-import { getUserImageSrc } from '../../services/imageService'
-import PostCard, {fetchPosts} from '../../components/PostCard'
-import Loading from '../../components/Loading'
-import {getUserData} from '../../services/userService'
-import Avatar from '../../components/Avatar'
 import { supabase } from '../../lib/supabase'
-
+import { useAuth } from '../../contexts/AuthContext'
+import {Svg, Circle, Path} from 'react-native-svg';
+import { theme } from '../../constants/theme'
+import Icon from '../../assets/icons'
+import { Image } from 'expo-image'
+import { getUserImageSrc } from '../../services/imageService'
+import { hp, wp } from '../../helpers/common'
+import { useRouter } from 'expo-router'
+import { fetchPosts } from '../../services/postService'
+import PostCard from '../../components/PostCard'
+import Loading from '../../components/Loading'
+import { getUserData } from '../../services/userService'
+import Avatar from '../../components/Avatar'
 var limit = 0;
 const HomeScreen = () => {
     const {user, setAuth} = useAuth();
@@ -110,8 +110,9 @@ const HomeScreen = () => {
         setPosts(res.data);
       }
     }
-    return (
-        <ScreenWrapper bg="white">
+
+  return (
+    <ScreenWrapper bg="white">
       <View style={styles.container}>
         {/* header */}
         <View style={styles.header}>
@@ -180,66 +181,65 @@ const HomeScreen = () => {
       
 
 </ScreenWrapper>
-    )
+  )
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    },
-
-    header: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'space-between',
-        marginBottom: 10,
-        marginHorizontal: wp(4)
-    },
-    title: {
-        color: theme.colors.text,
-        fontSize: hp(3.2),
-        fontWeight: theme.fonts.bold
-    },
-    avatarImage: {
-        height: hp(4.3),
-        width: hp(4.3),
-        borderRadius: theme.radius.sm,
-        borderCurve: 'continuous',
-        borderColor: theme.colors.gray,
-        borderWidth: 3
-    },
-    icons: {
-        flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'center',
-        gap: 18
-    },
-    listStyle: {
-        paddingTop: 20,
-        paddingHorizontal: wp(4)
-    },
-    noPosts: {
-        fontSize: hp(2),
-        textAlign: 'center',
-        color: theme.colors.text
-    },
-    pill: {
-        position: 'absolute',
-        right: -10,
-        top: -4,
-        height: hp(2.2),
-        width: hp(2.2),
-        justifyContent: 'center',
-        alignItems: 'center',
-        borderRadius: 20,
-        backgroundColor: theme.colors.roseLight
-    },
-    pillText: {
-        color: 'white',
-        fontSize: hp(1.2),
-        fontWeight: theme.fonts.bold
-    }
-
+  container: {
+    flex: 1,
+    // paddingHorizontal: wp(4)
+  },
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginBottom: 10,
+    marginHorizontal: wp(4)
+  },
+  title: {
+    color: theme.colors.text,
+    fontSize: hp(3.2),
+    fontWeight: theme.fonts.bold
+  },
+  avatarImage: {
+    height: hp(4.3),
+    width: hp(4.3),
+    borderRadius: theme.radius.sm,
+    borderCurve: 'continuous',
+    borderColor: theme.colors.gray,
+    borderWidth: 3
+  },
+  icons: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: 18
+  },
+  listStyle: {
+    paddingTop: 20,
+    paddingHorizontal: wp(4)
+  },
+  noPosts: {
+    fontSize: hp(2),
+    textAlign: 'center',
+    color: theme.colors.text
+  },
+  pill: {
+    position: 'absolute',
+    right: -10,
+    top: -4,
+    height: hp(2.2),
+    width: hp(2.2),
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 20,
+    backgroundColor: theme.colors.roseLight
+  },
+  pillText: {
+    color: 'white',
+    fontSize: hp(1.2),
+    fontWeight: theme.fonts.bold
+  }
 })
 
-export default HomeScreen;
+export default HomeScreen
